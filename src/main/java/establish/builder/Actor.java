c72ff18cfcec7533968863b21593a0efc108fb98
wusd <wsd5656126@foxmail.com>
2020-01-12 21:08:26 +0800

    --建造者模式

---
+++ src/main/java/establish/builder/Actor.java
@@ -0,0 +1,19 @@
+package establish.builder;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * 产品类
+ * @author wusd
+ * @date 2020/1/12 1:03
+ */
+@Data
+@ToString
+public class Actor {
+    private String type;
+    private String sex;
+    private String face;
+    private String costume;
+    private String hairstyle;
+}
