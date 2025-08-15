dd0f0e863234e777818ac871109bf7ea4ed115b1
wusd <nanxihope@163.com>
2025-08-15 17:21:28 +0800

    observer pattern

---
+++ src/main/java/headfirst/c02/wrong/WeatherData.java
@@ -0,0 +1,24 @@
+package headfirst.c02.wrong;
+
+/**
+ * @author Wusd
+ * @date 2025/8/15
+ * @description
+ */
+// 错误示范
+public class WeatherData {
+    // 实例变量声明...
+
+    public void measurementsChanged() {
+//        float temperature = getTemperature();
+//        float humidity = getHumidity();
+//        float pressure = getPressure();
+//
+//        // 更新布告板
+//        currentConditionsDisplay.update(temperature, humidity, pressure);
+//        statisticsDisplay.update(temperature, humidity, pressure);
+//        forecastDisplay.update(temperature, humidity, pressure);
+    }
+
+    // ...其他方法
+}
